0ed97df2255247a9b63798a030fe183ffed75480
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-16 21:10:50 -0400

    removed test_create_new_admin_user, created db_admin_fixture.json to store admin information, created db_fixture_setup to call the db_admin_fixture.json,

--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -14,3 +14,14 @@ def create_admin_user(django_user_model):
 
 
 
+@pytest.fixture(scope="session")
+def db_fixture_setup(django_db_setup, django_db_blocker):
+    """
+    Load DB data fixtures
+    """
+    with django_db_blocker.unblock():
+        call_command("loaddata", "db_admin_fixture.json")
+       
+
+
+
