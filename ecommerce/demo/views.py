e33531083f3f584a491eec826cf8d7104bf7ff77
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-17 21:20:23 -0400

    added categories url, view, and template,

--- ecommerce/demo/views.py
+++ ecommerce/demo/views.py
@@ -5,3 +5,10 @@ from ecommerce.inventory import models
 def home(request):
 
     return render(request, "index.html")
+
+
+def category(request):
+
+    data = models.Category.objects.all()
+
+    return render(request, "categories.html", {"data": data})
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "kitty121",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
